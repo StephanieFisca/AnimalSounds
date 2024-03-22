bf4bd993fcab195218b77f61659f35625226c7f7
Stephanie Fisca <stephaniefisca02@gmail.com>
2024-03-22 22:06:55 +0200

    Initial implementation

--- AnimalSounds/Program.cs
+++ AnimalSounds/Program.cs
@@ -3,43 +3,24 @@ using System.Collections.Generic;
 
 namespace AnimalSounds
 {
-    // Animal class
+    /* Animal Class */
     class Animal
     {
-        // Properties
+        /* Class properties */
         public string Type { get; }
         public string Sound { get; }
 
-        /*Constructor with parameters that initialises the type and sound
-        of an animal */
+        /* Constructor that initialises the type and sound of an animal */
         public Animal(string type, string sound)
         {
             Type = type;
             Sound = sound;
         }
 
-        // Method to make the animal sound
+        /* Method that makes the animal sound */
         public void MakeSound()
         {
-            Console.WriteLine($"{Type} makes sound: {Sound}");
-        }
-
-    }
-
-    // MatchSoundToAnimal class to generate animal sounds
-    class MatchSoundToAnimal
-    {
-        //Generates the sound an animal makes
-        public static List<Animal> GenerateAnimalSounds()
-        {
-            return new List<Animal>
-            {
-                new Animal("Dog", "Bark"),
-                new Animal("Snake", "Ssss"),
-                new Animal("Horse", "Neigh"),
-                new Animal("Cat", "Meow"),
-                new Animal("Hyena", "Laugh")
-            };
+            Console.WriteLine($"{Type} makes the sound {Sound}");
         }
     }
 
@@ -47,10 +28,15 @@ namespace AnimalSounds
     {
         static void Main(string[] args)
         {
-            // Get the list of animals with sounds
-            List<Animal> animals = MatchSoundToAnimal.GenerateAnimalSounds();
+            /* Create list of animals and their sounds */
+            List<Animal> animals = new List<Animal>
+            {
+                new Animal("Dog", "Bark"),
+                new Animal("Cat", "Meow"),
+                new Animal("Elephant", "Trumpet")
+            };
 
-            // Iterate through all animals and generates their corresponding sound
+            /* Iterate through each animal and make the corresponding sound */
             foreach (var animal in animals)
             {
                 animal.MakeSound();

c1d1ac502273d4c4913fd3849c72a05e0cd215f7
Stephanie Fisca <stephaniefisca02@gmail.com>
2024-03-28 17:04:03 +0200

    Added IAnimal and seperate files for classes

---
+++ AnimalSounds/Animal.cs
@@ -0,0 +1,29 @@
+﻿using AnimalSounds.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSounds
+{
+    public class Animal : IAnimal
+    {
+            /* Animal Class Properties */
+            public string Type { get; }
+            public string Sound { get; }
+
+            /* Constructor that initialises the type and sound of an animal */
+            public Animal(string type, string sound)
+            {
+                Type = type;
+                Sound = sound;
+            }
+
+            /* Method that makes the animal sound */
+            public void MakeSound()
+            {
+                Console.WriteLine($"{Type} makes the sound {Sound}");
+            }
+        }
+}
---
+++ AnimalSounds/MatchSound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSounds
+{
+    /* MatchSound class to generate animal sounds */
+    public abstract class MatchSound
+    {
+        /* Generates the sound an animal makes */
+        public static List<Animal> GenerateAnimalSounds()
+        {
+            /* Returns a list of animals and their sounds */
+            return new List<Animal>
+            {
+                new Animal("Dog", "Bark"),
+                new Animal("Snake", "Ssss"),
+                new Animal("Horse", "Neigh"),
+                new Animal("Cat", "Meow"),
+                new Animal("Hyena", "Laugh")
+            };
+        }
+    }
+}
